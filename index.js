f28ede49a0d812269a5fad1d591380149a0e7f78
ndurushima <ndu1994@gmail.com>
2025-06-11 18:54:47 -0500

    Lab merge users first attempt

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+    merge_date: Date.today().toString('M/d/yyyy')
+  };
 
+  for (const userArray of args) {
+    combinedObject.users.push(...userArray);
+  }
+
+  return combinedObject;
+};
+
+const groupA = ['Alicia', 'Dave'];
+const groupB = ['Emily', 'Ashley']; 
+
+console.log(combineUsers(groupA, groupB));
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+
